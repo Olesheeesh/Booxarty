d15e1f2ba1229cc2978c04d677d26e890c527ee6
Olesheeesh <olezha17kravets@gmail.com>
2023-12-09 19:24:14 +0200

    bebra2

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Bebra");
         System.out.println("Bebra2");
         System.out.println("BebraDofiga");
+        System.out.println("BebraDofiga2");
     }
 }
